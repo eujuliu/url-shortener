90f33bca14b3021ac28f1ee6e465bc6a304cbeb1
Julio Martins <juliomarttins5@gmail.com>
2025-11-14 20:36:01 -0300

    chore: add initial config files

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+async function main() {
+  const app = new Hono();
+
+  app.get("/", (c) => {
+    return c.text("Hello Hono!");
+  });
+
+  serve(
+    {
+      fetch: app.fetch,
+      port: 3000,
+    },
+    (info) => {
+      console.log(`Server is running on http://localhost:${info.port}`);
+    },
+  );
+}
+
+main().catch(console.error);
